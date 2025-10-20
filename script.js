ac63b3f66627a2239046b609487cd26aeaf8aa98
Kadisa-Cyber <kauan.d.santos10@aluno.senai.br>
2025-10-20 09:43:17 -0300

    CHALLENGE - Feature 1

--- script.js
+++ script.js
@@ -10,6 +10,24 @@ function btnMin() {
   resultado.value = input.value.toLowerCase();
 }
 
+function btnCap() {
+  const input = document.getElementById("input-text");
+  const resultado = document.getElementById("textAreaResult");
+
+  let valorAtual = input.value;
+  if (valorAtual.length > 0) {
+    let primeiraLetra = valorAtual.charAt(0).toUpperCase();
+    let restante = valorAtual.slice(1).toLowerCase();
+    resultado.value = primeiraLetra + restante;
+  }
+}
+
+function btnClr() {
+  const input = document.getElementById("input-text");
+  const resultado = document.getElementById("textAreaResult");
+  resultado.value = input.value.toLowerCase();
+}
+
 function abrirLinkGithub(event) {
   event.preventDefault();
   chrome.tabs.create({ url: "https://github.com/Kadisa-Cyber" });
@@ -18,6 +36,8 @@ function abrirLinkGithub(event) {
 document.addEventListener("DOMContentLoaded", () => {
   document.getElementById("btn-max").addEventListener("click", btnMai);
   document.getElementById("btn-min").addEventListener("click", btnMin);
+  document.getElementById("btn-cap").addEventListener("click", btnCap);
+  document.getElementById("btn-clr").addEventListener("click", btnClr);
   document
     .getElementById("link-github")
     .addEventListener("click", abrirLinkGithub);
